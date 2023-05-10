a5ae1b826ba1f3ad2086b971c84f4fef18bb5210
Enmanuel Rodriguez <enmanuelrodmac@gmail.com>
2023-05-10 16:49:08 -0400

    conexion a la base de datos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,6 @@
-const { response, request } = require('express')
+const { response, request } = require('express');
 
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
     const { nombre = 'no name', apikey, page = 1, limit } = req.query
@@ -13,6 +14,19 @@ const usuariosGet = (req = request, res = response) => {
 
 }
 
+const usuariosPost = async(req, res) => {
+
+    const body = req.body
+
+    const usuario = new Usuario(body)
+
+    await usuario.save();
+    res.json({
+        msj: 'api post',
+        usuario
+    })
+}
+
 const usuariosPut = (req, res) => {
     const { id } = req.params
     res.json({
@@ -21,13 +35,6 @@ const usuariosPut = (req, res) => {
     })
 }
 
-const usuariosPost = (req, res) => {
-    const body = req.body
-    res.json({
-        msj: 'api post',
-        body
-    })
-}
 
 const usuariosDelete = (req, res) => {
     res.json({
--- models/server.js
+++ models/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-
         // Patch
         this.userPatch = '/api/usuarios';
 
-
+        //Conectar a base de datos
+        this.conectarDB();
+        
         //Middlewares
         this.middlewares();
 
 
         //Rutas de mi aplicacion
         this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middlewares() {
